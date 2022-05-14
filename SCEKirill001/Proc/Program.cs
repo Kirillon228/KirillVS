80da970abaa60d059d26b5635e7f4aebd5c3b858
Kirillon228 <kirillkir3118@gmail.com>
2022-05-14 20:02:07 +0300

    init commit

---
+++ SCEKirill001/Proc/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double b = 0;
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write("Введите А:");
+                double aa = Convert.ToDouble(Console.ReadLine());
+                b = Power3A(aa);
+                Console.WriteLine($"Ответ:{b}");
+            }
+            Console.Read();
+        }
+
+        private static double Power3A(double ab)
+        {
+            return Math.Pow(ab, 3);
+        }
+    }
+}
---
+++ SCEKirill001/Proc6/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proc6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("Введите числа:");
+                int k = Convert.ToInt32(Console.ReadLine());
+
+                int DigitCount = 0;
+                int SumDigit = 0;
+
+                (DigitCount, SumDigit) = DigitCountSum(k);
+
+                Console.WriteLine($"Сумма чисел равна:{SumDigit}\nКоличество чисел:{DigitCount}\n");
+                Console.Write("Продолжить работу программы? [0] => No, [1] => Yes\nВаш ответ: ");
+                int answer = Convert.ToInt32(Console.ReadLine());
+
+                if (answer != 1)
+                {
+                    Console.WriteLine("Конец программы");
+                    Console.Read();
+                    break;
+                }
+            }
+
+        }
+        private static (int, int) DigitCountSum(int Number)
+        {
+            int DigitCount = 0;
+            int SumDigit = 0;
+
+            while (Number > 0)
+            {
+                SumDigit += Number % 10;
+                DigitCount++;
+                Number /= 10;
+            }
+
+            return (DigitCount, SumDigit);
+            
+        }
+    }
+}
