6f1bb7d6b5c1bc046f70e2a027b234b0f09bf1b4
Kirillon228 <kirillkir3118@gmail.com>
2022-05-22 23:24:51 +0300

    Proc 30

---
+++ SCEKirill001/Proc30_1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proc30
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write("Введите число:");
+                int k = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("Введите число:");
+                int n = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine(DigitN(k, n));
+            }
+            Console.Read();
+
+        }
+        private static int DigitN(int k, int n)
+        {
+
+            for (int i = 0; k > 0; i++)
+            {
+                if (i == n)
+                {
+                    return k % 10;
+
+                }
+                k /= 10;
+            }
+            return -1;
+        }
+    }
+}
