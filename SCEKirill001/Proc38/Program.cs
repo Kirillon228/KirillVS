de130ba30552524785d8b85126f5e24d692394a2
Kirillon228 <kirillkir3118@gmail.com>
2022-05-24 21:52:12 +0300

    Proc 38

---
+++ SCEKirill001/Proc38/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proc38
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число:");
+            double a = Convert.ToDouble(Console.ReadLine());
+
+            for (int i = 0; i < 3; i++)
+            {
+                Console.Write("Введите степень числа:");
+                int n = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine($"Ответ:{Power2(a,n)}");
+            }
+            Console.Read();
+
+        }
+        private static double Power2(double a,int n)
+        {
+            a = Math.Pow(a, Math.Abs(n));
+            if (n < 0)
+            {
+                a = 1 / a;
+            }       
+            return a;
+        }
+    }
+}
