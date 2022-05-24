7f0d0bb0e51ada57c8750014c74e7a206acac20d
Kirillon228 <kirillkir3118@gmail.com>
2022-05-24 18:45:26 +0300

    Proc 37

---
+++ SCEKirill001/Proc37/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proc37
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите степень числа:");
+            double p = Convert.ToDouble(Console.ReadLine());
+
+            double answer = 0;
+
+            for(int i = 0; i < 3; i++)
+            {
+                answer = 0;
+
+                Console.Write("Введите число:");
+                double a = Convert.ToDouble(Console.ReadLine());
+
+                if(a < 1)
+                {
+                    Console.WriteLine($"ответ:{answer}");
+                }
+                else
+                {
+                    Console.WriteLine($"ответ:{Power1(a,p)}");
+                }
+            }
+        }
+        private static double Power1(double a, double p)
+        {
+            
+            return Math.Exp(p * Math.Log(a));
+        }
+    }
+}
