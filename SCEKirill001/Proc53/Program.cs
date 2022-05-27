aa20fb510795a9adbd0c5f73360836da7d2482b1
Kirillon228 <kirillkir3118@gmail.com>
2022-05-27 17:06:23 +0300

    Proc 56

---
+++ SCEKirill001/Proc53/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proc53
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите год:");
+            int year = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < 3; i++)
+            {
+                Console.Write("Введите месяц:");
+                int month = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine(IsLeapYear(year, month));
+                
+            }
+            Console.Read();
+        }
+        private static int IsLeapYear(int year, int month)
+        {          
+
+            if (year % 4 == 0)
+            {                
+                if (year % 100 == 0)
+                {
+                    if (year % 400 == 0)
+                    {
+                        year = 366;
+                    }
+                    else
+                    {
+                        year = 365;
+                    }
+                }
+                else
+                {
+                    year = 366;
+                }
+            }
+            else
+            {
+                year = 365;
+            }
+
+            switch (month)
+            {
+                case 1: return 31;
+                case 2 when year == 365: return 28;
+                case 2 when year == 366: return 29;  
+                case 3: return 31;
+                case 4: return 30;
+                case 5: return 31;
+                case 6: return 30;
+                case 7: return 31;
+                case 8: return 31;
+                case 9: return 30;
+                case 10: return 31;
+                case 11: return 30;
+                case 12: return 31;
+                default: return 0;
+            }
+           
+        } 
+    }
+}
---
+++ SCEKirill001/Proc56/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proc56
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите 1 точку:");
+            double ax = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Введите 1 точку:");
+            double ay = Convert.ToDouble(Console.ReadLine());
+
+            for (int i= 0;i < 3; i++)
+            {
+                Console.Write("Введите 2 точку:");
+                double bx = Convert.ToDouble(Console.ReadLine());
+
+                Console.Write("Введите 2 точку:");
+                double by = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine($"Ответ:{Leng(ax, ay, bx, by)}");
+                
+            }
+            Console.Read();
+        }
+        private static double Leng(double ax, double ay, double bx, double by) 
+        {
+            double answer = Math.Sqrt(Math.Pow((ax - bx), 2) + Math.Pow((ay - by), 2));
+            return Math.Abs(answer);
+        }
+
+    }
+}
