dc2ccd1583bf67a5f3f2d58d6adba51ba67c3c9a
Kirillon228 <kirillkir3118@gmail.com>
2022-05-27 22:54:42 +0300

    MaxMin 1

---
+++ SCEKirill001/MaxMin1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaxMin1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите количество чисел:");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Введите число:");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            int answer = a;
+            int answer2 = a;
+
+            for (int i = 1; i < n; i++) 
+            {
+                Console.Write("Введите число:");
+                int b = Convert.ToInt32(Console.ReadLine());
+                if (b > answer) 
+                {
+                    answer = b;
+
+                }
+                if (b < answer2) 
+                {
+                    answer2 = a;
+                }              
+            }
+            Console.WriteLine($"Max:{answer}");
+            Console.WriteLine($"Min:{answer2}");
+            Console.Read();
+        }
+    }
+}
