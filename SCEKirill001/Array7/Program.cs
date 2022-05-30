2c34fb3331d824c5ef7063d4e1951af050396f4b
Kirillon228 <kirillkir3118@gmail.com>
2022-05-30 13:10:16 +0300

    Array 7

---
+++ SCEKirill001/Array7/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите n:");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int[] array = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("Введите числа:");
+                array[i] = Convert.ToInt32(Console.ReadLine());
+
+            }
+            for (int j = n - 1; j >= 0; j--) 
+            {
+                Console.WriteLine($"Ответ:{array[j]}");
+            }
+            Console.Read();
+
+        }
+    }
+}
